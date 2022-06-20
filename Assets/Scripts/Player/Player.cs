2ec101344962145204589a4ef6082c35f748f27b
Rodrigo Passos Sundfeld <rodrigo.psundfeld@gmail.com>
2022-06-20 15:36:01 -0300

    Adding Run Key

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float speedRun;
     public float forceJump;
+
+    private float _currentSpeed;
+    private bool _isRunning = false;
     private void Update()
     {
         HandleJump();
@@ -17,15 +21,25 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        //ESSE É BACANA
+        // if (Input.GetKey(KeyCode.LeftShift))
+        // {
+        //     _currentSpeed = speedRun;
+        // }
+        // else
+        // {
+        //     _currentSpeed = speed;
+        // }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime); //possição do personagem + a posição que ele quer andar + deltaTime para normalizar o tempo
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(Input.GetKey(KeyCode.LeftShift) ? speedRun : speed, myRigidbody.velocity.y); //IF TERNÁRIO, BRABO
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(Input.GetKey(KeyCode.LeftShift) ? -speedRun : -speed, myRigidbody.velocity.y);
         }
 
         if (myRigidbody.velocity.x > 0)
